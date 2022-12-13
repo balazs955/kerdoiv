e5392619c50a7bbbacb95ed622a3da8f22f8a01c
Berc57 <83001491+Berc57@users.noreply.github.com>
2022-12-13 12:29:13 +0100

    Add files via upload

--- urlap.js
+++ urlap.js
@@ -4,7 +4,7 @@ function mennyiJo() {
     var elsoValasz = document.getElementById("csapat").value;
     var validValaszEgy = "16";
     if(elsoValasz == validValaszEgy) {
-        count = count+1;
+        count = count + 1;
     }
     var masodikValasz = document.getElementById("kieso").value;
     var validValaszKetto = "Amerika";
@@ -12,13 +12,23 @@ function mennyiJo() {
     var validValaszKetto3 = "amerika";
     var validValaszKetto4 = "usa";
     if(masodikValasz == validValaszKetto || masodikValasz == validValaszKetto2 || masodikValasz == validValaszKetto3 || masodikValasz == validValaszKetto4) {
-        count = count+1;
+        count = count + 1;
     }
     var harmadikValasz = document.getElementById("nap").value;
     var validValaszHarom = "18";
     if(harmadikValasz == validValaszHarom) {
-        count = count+1;
+        count = count + 1;
     }
+    if (document.getElementById("elsoI").checked) {
+        count = count + 1;
+    }
+    if (document.getElementById("masodikH").checked) {
+        count = count + 1;
+    }
+    if (document.getElementById("harmadikI").checked) {
+        count = count + 1;
+    }
+
     document.getElementById("count").innerHTML = count;
     return false;
 }
